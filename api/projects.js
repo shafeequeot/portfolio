25652bd1e38d1826037cbfdc7bde3476e780290e
Shafeeque OT <shafeequeot@gmail.com>
2022-08-09 23:28:19 +0400

    image fixing 8

--- api/projects.js
+++ api/projects.js
@@ -3,77 +3,77 @@ const projects = { frameWorks: [
        name: 'Netflix',
        Url: 'https://github.com/shafeequeot/netflix-reactApp',
        discription: "Clone netflix with React js js, axios and API from https://api.themoviedb.org/3",
-       image:'/images/netflix.jpg',
+       image:'/images/netflix.JPG',
        tag: ['React js', 'axios']
    },
     {
        name: 'Linkedin',
        Url: 'https://github.com/shafeequeot/linkedin',
        discription: "Clone Linkedin Social media website with React js js and Firebase backend",
-       image:'/images/linkedin.jpg',
+       image:'/images/linkedin.JPG',
        tag: ['React js', 'Firebase']
    },
    {
        name: 'Book My Move',
        Url: 'https://github.com/shafeequeot/mastermovers',
        discription: "Book My move website developed for mastermovers.ae to booking moving services. It is ongoing project, waiting for complete backend to produce",
-       image:'/images/bookmymove.jpg',
+       image:'/images/bookmymove.JPG',
        tag: ['React js', 'Tailwind CSS']
    },
    {
        name: 'Portfolio',
        Url: 'https://github.com/shafeequeot/portfolio',
        discription: "Portfolio website developed with React jsjs, Redux, Redux tool kit, Tailwind CSS",
-       image:'/images/portolio.jpg',
+       image:'/images/portolio.JPG',
        tag: ['React js', 'Tailwind CSS', 'Redux', 'Redux tool kit']
    },
    {
        name: 'Moble Service Tracker',
        Url: 'https://github.com/shafeequeot/Mobile-Service-Tracker',
        discription: "Mobile service tracking system developed in Electron js js, data table and fs-extra as desktop application for mobile whole sale company",
-       image:'/images/mobileservice.jpg',
+       image:'/images/mobileservice.JPG',
        tag: ['Electron js', 'data table', 'fs-extra']
    }, 
    {
        name: 'Shopping cart',
        Url: 'https://github.com/shafeequeot/Shopping-Cart-with-Node-express',
        discription: "developed a dummy shopping cart for learning node js, node-express, and mongo db",
-       image:'/images/shoppingcart.jpg',
+       image:'/images/shoppingcart.JPG',
        tag: ['Node js', 'express', 'mongo db']
    }, 
    {
        name: 'Auxwall Gym Management',
        Url: 'https://github.com/shafeequeot/Auxwall',
        discription: "Desktopo application for gym membership, membership renewal, sale and purchase with accounting reports",
-       image:'/images/auxwall.jpg',
+       image:'/images/auxwall.JPG',
        tag: ['Electron js', 'tabulator', 'sqlite', 'fs-extra']
    }, 
    {
        name: 'mfost.net',
        Url: 'https://mfost.net/',
        discription: "Website for SEO and Webdesign as freelancer",
-       image:'/images/mfost.jpg',
+       image:'/images/mfost.JPG',
        tag: ['Wordpress']
    }, 
    {
        name: 'Master Movers LLC',
        Url: 'https://mastermovers.ae/',
        discription: "Website for Master movers LLC to showcase their services and products",
-       image:'/images/mastermovers.jpg',
+       image:'/images/mastermovers.JPG',
        tag: ['Wordpress']
    }, 
    {
        name: 'Muzaco Print & Pack',
        Url: 'https://muzacopack.com/',
        discription: "Website for Muzaco print and pack to showcase their services and products",
-       image:'/images/muzaco.jpg',
+       image:'/images/muzaco.JPG',
        tag: ['Wordpress']
    }, 
    {
        name: 'Aux Kart',
        Url: 'https://auxkart.com/',
        discription: "E-commerce website for selling mobile phones. developed in wordpress & woocommerce",
-       image:'/images/auxkart.jpg',
+       image:'/images/auxkart.JPG',
        tag: ['Wordpress', 'Woocommerce']
    }
 
@@ -83,14 +83,14 @@ const projects = { frameWorks: [
        name: 'Adobe Illusatrator',
        Url: 'https://github.com/shafeequeot/netflix-reactApp',
        discription: "Clone netflix with React js js, axios and API from https://api.themoviedb.org/3",
-       image:'/images/netflix.jpg',
+       image:'/images/netflix.JPG',
        tag: ['Adobe Illustrator']
    },
    {
        name: 'Adobe Photoshop',
        Url: 'https://github.com/shafeequeot/netflix-reactApp',
        discription: "Clone netflix with React js js, axios and API from https://api.themoviedb.org/3",
-       image:'/images/netflix.jpg',
+       image:'/images/netflix.JPG',
        tag: ['Adobe Photoshop']
    }
 
--- components/reUsable/viewWebItem.js
+++ components/reUsable/viewWebItem.js
@@ -35,10 +35,10 @@ function ViewItem() {
       <div className='bg-dark-500  '>
       <div class="group cursor-pointer flex flex-col">
         <div className=' w-44  overflow-hidden'>
-        <img width={330} height={220} loader={imageLoader} className='object-fill' src={key.image} alt={key.name}></img>
+        <Image width={330} height={220} loader={imageLoader} className='object-fill' src={key.image} alt={key.name}></Image>
         </div>
         <div class="fixed ease-in-out duration-700 bottom-1/2 left-1/2 hidden  h-1/2 w-1/2 bg-gray-200 z-50 shadow-lg rounded object-cover  group-hover:block ...">
-        <img width={700} height={500} loader={imageLoader} className='w-full' src={key.image} alt={key.name}></img>
+        <Image width={700} height={500} loader={imageLoader} className='w-full' src={key.image} alt={key.name}></Image>
         </div>
       </div>
         
