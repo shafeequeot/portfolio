41ae735b332e1d596ba28b0b81d68f12b0a27450
shafeeque <shafeequeot@gmail.com>
2022-08-09 13:30:53 +0400

    image fixing

--- components/reUsable/item.js
+++ components/reUsable/item.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { changeItem } from '../../redux/portfolioSlice'
 import allprojects from '../../api/projects'
+
 function Item(props) {
+
+
+
 
     const dispatch = useDispatch()
     const clickedItem = (e) => {
@@ -45,7 +49,7 @@ function Item(props) {
 
                 <div className='p-2 bg-dark-800 rounded border  border-dark-600 peer-checked:border-dark-200 hover:border-dark-200 cursor-pointer flex flex-row gap-4 items-center'>
                     <div className='flex items-center justify-center overflow-hidden rounded-full flex-shrink-0 bg-dark-500 h-16 w-16'>
-                        <img className='object-contain h-12 w-12' src={props.avatar} alt={props.title} />
+                        <img  className='object-contain h-12 w-12' src={`${props.avatar}`} alt={props.title} />
                     </div>
                     <div>
                         <div className='text-base uppercase font-medium '>
