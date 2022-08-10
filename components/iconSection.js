68508337450d728936c3dfb094da7c36606ddcbe
Shafeeque OT <shafeequeot@gmail.com>
2022-08-11 00:42:29 +0400

    some bug fixed

--- components/iconSection.js
+++ components/iconSection.js
@@ -10,7 +10,7 @@ function IconSection() {
   return (
   
 
-      <div id='iconList' className='grid  grid-cols-5 md:grid-cols-6 lg:flex lg:flex-row self-center m-auto   scroll-smooth gap-4 overflow-hidden justify-center justify-items-center'>
+      <div id='iconList' className='grid grid-cols-5 md:grid-cols-6 lg:flex lg:flex-row self-center m-auto   scroll-smooth gap-2 lg:gap-4 overflow-hidden justify-center '>
 
         <Icon url="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"/>
         <Icon url="https://miro.medium.com/max/1200/1*O6KluMvEBZ1cBL3EPo4tig.png"/>
